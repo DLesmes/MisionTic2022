ec58f5c543da0abab4af74e03fdfac02456aae05
DLesmes <ing.dlesmes@gmail.com>
2021-05-09 21:48:01 -0500

    Reto semana 1 solucionado y adaptado

--- retos/reto#2/reto_semana1.py
+++ retos/reto#2/reto_semana1.py
@@ -4,8 +4,8 @@ def budget(salary):
     couple = salary*0.1
     piggy = salary*0.05
     market = salary - (rent+services+couple+piggy)
-    print( 'Dinero dedicado para mercar ${0}'.format(market))
+    print('{0}'.format(market))
 
 if __name__ == "__main__":
-    salary = int(input('cual es tu salario mensual?: '))
+    salary = int(input())
     budget(salary)
