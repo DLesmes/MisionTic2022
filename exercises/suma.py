ec8fa873866edd1f59e5218eff6d7df6f6c5e4b5
DLesmes <ing.dlesmes@gmail.com>
2021-05-11 18:59:21 -0500

    Book tema 2

--- exercises/suma.py
+++ exercises/suma.py
@@ -1,7 +1,7 @@
 def suma(a,b):
-    print(a+b)
+    print('El resultado es' + str(a+b))
 
-if __name__== '__main__':
+if __name__== "__main___":
     a = int(input('Give me the first add :'))
-    b = int(input('Give me the first add'))
+    b = int(input('Give me the second add :'))
     suma(a,b)
