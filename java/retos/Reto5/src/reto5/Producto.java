1b0d90be1f8b84f0ae49a30d3948d531e9aa0a88
DLesmes <ing.dlesmes@gmail.com>
2021-08-01 16:59:56 -0500

    challenge 5 started

---
+++ java/retos/Reto5/src/reto5/Producto.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto5;
+
+/**
+ *
+ * @author asf
+ */
+public class Producto {
+    
+    int id;
+    String nombre;
+    int cantidad;
+    String categoria;
+    double precio;
+
+    public Producto(String nombre, int cantidad, String categoria, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public Producto(int id, String nombre, int cantidad, String categoria, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
