1a4c9225ca858b28130c5f30e7109e5ff5c6cdd0
Diego Lesmes <61529697+DLesmes@users.noreply.github.com>
2021-09-21 19:49:37 -0500

    Update Conexion_bd.java
    Given code for databae conexion

--- SoftwareEngineering/projectsample/src/main/java/persistencia/Conexion_bd.java
+++ SoftwareEngineering/projectsample/src/main/java/persistencia/Conexion_bd.java
@@ -11,54 +11,67 @@ package persistencia;
  */
 import java.sql.*;
 
-public class Conexion_bd {
-    
-    private String url="";
-    private String DB_driver="";
-    private String host="";
-    private String db="";
-    private String username="";
-    private String password="";
-    public Connection con=null;
-    private Statement stmt=null;
-    private ResultSet rs=null;
+public class ConexionBD {
+
+    private String url = "";
+    private String DB_driver = "";
+    private String host = "";
+    private String db = "";
+    private String username = "";
+    private String password = "";
+    public Connection con = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
     private boolean local;
 
-    public ConexionBD(){
-    
-        local=false;
-        DB_driver="com.mysql.jdbc.Driver";
-        if (local){
-            
-            host="167.99.168.84:3306";
+    public ConexionBD() {
+
+        local = false;
+        DB_driver = "com.mysql.jdbc.Driver";
+        if (local) {
+            host = "localhost:3306";
             db = "S33Prueba";
-            url="jdbc:mysql://"+host+"/"+db;
-            username="root";
-            password="root";
-            
+            url = "jdbc:mysql://" + host + "/" + db;
+            username = "root";
+            password = "root";
         } else {
-            host="167.99.168.84:3306";
-            String db = "S33Prueba";
-            url="jdbc:mysql://"+host+"/"+db;
-            username="mision";
-            password="tics";
-            
+            host = "167.99.168.84:3306";
+            db = "S33Prueba";
+            url = "jdbc:mysql://" + host + "/" + db;
+            username = "mision";
+            password = "tics";
+        }
+
         try {
-            
             Class.forName(DB_driver);
-            
-        } catch(ClassNotFoundException ex){
-            System.out.println("Error en el Driver de la base de datos" + ex);
-            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error en el driver de la base de datos: " + ex);
         }
-            
+
         try {
-            con =DriverManager.getConnection(url);
-            if (con != null)
-                DatabaseMetadata meta = con.getConnection()
-        } catch(){
-        
+            con = DriverManager.getConnection(url);
+            if (con != null) {
+                DatabaseMetaData meta = con.getMetaData();
+                System.out.println("Base de datos conectada " + meta.getDriverName());
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error en la conexión a la base de datos " + ex.getMessage());
         }
-        }
+
     }
+
+    public ResultSet consultarBD(String sentencia) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(sentencia);
+        } catch (SQLException sqlex) {
+            System.out.println(sqlex.getMessage());
+        } catch (RuntimeException rex) {
+            System.out.println(rex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return rs;
+    }
+
 }
